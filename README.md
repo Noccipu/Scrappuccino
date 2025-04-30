a78902c6c4fdd4b5ffe241925abf57fa03869dab
JackVerger <jackverger.art@gmail.com>
2025-04-30 22:35:01 +0200

    Created a multipage project structure

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Scrapuccino
+
+```python
+python -m venv venv
+```
+
+```python
+python.exe -m pip install --upgrade pip
+```
+
+```python
+pip freeze > requirements.txt
+```
+
+```python
+pip install -r requirements.txt
+```
---
+++ src/Scrappuccino.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Scrappuccino",
+    page_icon="☕"
+)
+
+st.write("# Welcome to Scrappuccino! 👋")
+
+st.sidebar.success("Select a page above.")
+
+st.markdown(
+    """
+    Presentation to write
+    """
+)
